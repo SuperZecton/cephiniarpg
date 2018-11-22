67db85866b4893c87c4a536a80ce730268bf5721
X1XX <dragonhearth1946@gmail.com>
2018-11-21 21:47:10 -0600

    ItemBuilder
    Added ItemBuilder

--- src/main/java/org/cephinia/core/abilities/AbilityGui.java
+++ src/main/java/org/cephinia/core/abilities/AbilityGui.java
@@ -4,19 +4,20 @@ package org.cephinia.core.abilities;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.entity.Fireball;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.util.Vector;
-import org.cephinia.core.utils.InventoryBuilder;
+import org.cephinia.core.utils.ItemBuilder;
 import org.cephinia.core.utils.Utils;
 
 public class AbilityGui {
 
     public static Inventory inv;
     public static String inventory_name;
-    public static int inv_rows = 1 * 9;
+    public static int inv_rows = 9;
 
     public static void initialize() {
         inventory_name = Utils.chat("&c&lMagical Book");
@@ -26,19 +27,18 @@ public class AbilityGui {
 
     public static Inventory GUI (Player player) {
         Inventory toReturn = Bukkit.createInventory(null, inv_rows, inventory_name);
-
-        InventoryBuilder.createItem(inv, 385, 1, 0, "&cFireball", "&b Costs 1 Mana", "&a Magical Skill");
-        InventoryBuilder.createItem(inv, 370, 1, 1, "&cDragonVortex", "&b Costs 3 Mana", "&a Magical Skill");
-        InventoryBuilder.createItem(inv, 381, 1, 8, "&cTeleport", "&b Costs 1 Mana", "&a Magical Skill");
+        toReturn.setItem(0, ItemBuilder.start(Material.FIREBALL).name("&cFireball").lore("&b Costs 1 Mana", "&a Magical Skill").build());
+        toReturn.setItem(1, ItemBuilder.start(Material.GHAST_TEAR).name("&cDragonVortex").lore("&b Costs 1 Mana", "&a Magical Skill").build());
+        toReturn.setItem(8, ItemBuilder.start(Material.EYE_OF_ENDER).name("&cTeleport").lore("&b Costs 1 Mana", "&a Magical Skill").build());
 
         for(int slots = 0; slots < inv.getSize();  slots++) {
             if(inv.getItem(slots) == null) {
-                InventoryBuilder.createItemByte(inv, 160, 14,1, slots, " ", " ");
-
+                inv.setItem(slots, ItemBuilder.start(Material.STAINED_GLASS_PANE).data((short) 14).name("").lore("").build());
 
             }
 
         }
+
 
         toReturn.setContents(inv.getContents());
         return toReturn;
